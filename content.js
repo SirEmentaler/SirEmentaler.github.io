297e92db88b1e5ca4cbc164bec6bf972423b2570
SirEmentaler <sir.ementaler@gmail.com>
2017-08-04 11:30:13 +0200

    Fade effect

--- content.js
+++ content.js
@@ -1,6 +1,19 @@
+$.effects.define("fade", "toggle", function(options, done) {
+	var show = options.mode === "show";
+	$(this)
+		.css("opacity", show ? 0 : 1)
+		.animate( {
+			opacity: show ? 1 : 0
+		}, {
+			queue: false,
+			duration: options.duration,
+			easing: options.easing,
+			complete: done
+		});
+});
 $(document).ready(function() {
 	$('a[href="#reference"]').click(function(event) {
-		$("#content").hide("fast", function() {
+		$("#content").hide("fast", "fade", function() {
 			$("#content").load("html/reference.html", function() {
 				$("#content").show("fast");
 			});
