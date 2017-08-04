e9f5e553dab844d6aa6554ebb90804119371af3e
SirEmentaler <sir.ementaler@gmail.com>
2017-08-04 14:27:46 +0200

    Load page initially

--- content.js
+++ content.js
@@ -1,8 +1,8 @@
 var currentContent;
 function loadContent(page) {
-	if (page !== currentContent) {
+	if (page.length > 0 && page !== currentContent) {
 		$("#content").hide("fade", "fast", function() {
-			$("#content").load("html/" + page + ".html", function() {
+			$("#content").load("html/" + page.substr(1) + ".html", function() {
 				currentContent = page;
 				$("#content").show("fade", "fast");
 			});
@@ -10,7 +10,10 @@ function loadContent(page) {
 	}
 }
 $(document).ready(function() {
+	if (!window.location.hash)
+		window.location.hash = "#home";
+	loadContent(window.location.hash);
 	$("a.load").click(function(event) {
-		loadContent(this.getAttribute("href").substr(1));
+		loadContent(this.getAttribute("href"));
 	});
 });
