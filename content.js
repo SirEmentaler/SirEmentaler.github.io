eef46843b0ea029a3f602a5569589a54dba847c8
SirEmentaler <sir.ementaler@gmail.com>
2017-08-04 11:41:16 +0200

    Use jQuery versions of the functions

--- content.js
+++ content.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 	$('a[href="#reference"]').click(function(event) {
-		$("#content").hide("fast", "fade", function() {
+		$("#content").hide("fade", "fast", function() {
 			$("#content").load("html/reference.html", function() {
-				$("#content").show("fast");
+				$("#content").show("fade", "fast");
 			});
 		});
 	});
