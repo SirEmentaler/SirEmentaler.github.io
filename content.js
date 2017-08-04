315b3a7669c5172d0726ccb85225eec70c8aa68e
SirEmentaler <sir.ementaler@gmail.com>
2017-08-04 11:15:43 +0200

    Add transition effects

--- content.js
+++ content.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
 	$('a[href="#reference"]').click(function(event) {
-		$("#content").load("html/reference.html");
-		event.preventDefault();
+		$("#content").hide("fast", function() {
+			$("#content").load("html/reference.html", function() {
+				$("#content").show("fast");
+			});
+		});
 	});
 });
