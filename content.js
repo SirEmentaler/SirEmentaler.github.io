185aae565d9d2aea7765f5a3c135da8ac845095d
SirEmentaler <sir.ementaler@gmail.com>
2017-08-04 16:04:58 +0200

    Fix hyperlinks

--- content.js
+++ content.js
@@ -1,10 +1,16 @@
 var currentContent;
+function prepareHyperlinks() {
+	$("a:not(.external)").click(function(event) {
+		loadContent(this.getAttribute("href"));
+	});
+}
 function loadContent(page) {
 	if (page.length > 0 && page !== currentContent) {
 		function onLoad() {
 			$("#content").load("html/" + page.substr(1) + ".html", function(responseText, textStatus, jqXHR) {
 				if (textStatus !== "error") {
 					currentContent = page;
+					prepareHyperlinks();
 					$("#content").slideDown("fast");
 				} else if (page !== "#404") {
 					loadContent("#404");
@@ -21,7 +27,4 @@ $(document).ready(function() {
 	if (!window.location.hash)
 		window.location.hash = "#home";
 	loadContent(window.location.hash);
-	$("a:not(.external)").click(function(event) {
-		loadContent(this.getAttribute("href"));
-	});
 });
