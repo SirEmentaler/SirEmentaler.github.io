36563f389d16c6864ef1b796431f89f3e4e46187
SirEmentaler <sir.ementaler@gmail.com>
2017-08-04 12:49:45 +0200

    Fix code

--- content.js
+++ content.js
@@ -10,6 +10,6 @@ function loadContent(page) {
 }
 $(document).ready(function() {
 	$("a.load").click(function(event) {
-		loadContent(this.attr("href").substr(1));
+		loadContent(this.href.substr(1));
 	});
 });
