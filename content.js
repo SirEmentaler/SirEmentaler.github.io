623e67409257088b77747a46e31ece707bf27118
SirEmentaler <sir.ementaler@gmail.com>
2017-08-04 11:36:09 +0200

    Use external source

--- content.js
+++ content.js
@@ -1,16 +1,3 @@
-$.effects.define("fade", "toggle", function(options, done) {
-	var show = options.mode === "show";
-	$(this)
-		.css("opacity", show ? 0 : 1)
-		.animate( {
-			opacity: show ? 1 : 0
-		}, {
-			queue: false,
-			duration: options.duration,
-			easing: options.easing,
-			complete: done
-		});
-});
 $(document).ready(function() {
 	$('a[href="#reference"]').click(function(event) {
 		$("#content").hide("fast", "fade", function() {
